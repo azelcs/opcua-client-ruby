7499798755f8b7ec0d548188c794bc3b5952eacd
Jelena Mihajlovic <jelena.mihajlovic@mitigate.dev>
2025-10-13 17:58:33 +0300

    Fix write type double

--- ext/opcua_client/opcua_client.c
+++ ext/opcua_client/opcua_client.c
@@ -936,16 +936,14 @@ static VALUE rb_writeUaValues(VALUE self, VALUE v_nsIndex, VALUE v_aryNames, VAL
             *(UA_Int64*)values[i].data = newValue;
             values[i].type = &UA_TYPES[uaType];
         } else if (uaType == UA_TYPES_FLOAT) {
-            Check_Type(v_newValue, T_FLOAT);
             UA_Float newValue = NUM2DBL(v_newValue);
             values[i].data = UA_malloc(sizeof(UA_Float));
             *(UA_Float*)values[i].data = newValue;
             values[i].type = &UA_TYPES[uaType];
         } else if (uaType == UA_TYPES_DOUBLE) {
-            Check_Type(v_newValue, T_FLOAT);
             UA_Double newValue = NUM2DBL(v_newValue);
             values[i].data = UA_malloc(sizeof(UA_Double));
-            *(UA_Float*)values[i].data = newValue;
+            *(UA_Double*)values[i].data = newValue;
             values[i].type = &UA_TYPES[uaType];
         } else if (uaType == UA_TYPES_BOOLEAN) {
             if (RB_TYPE_P(v_newValue, T_TRUE) != 1 && RB_TYPE_P(v_newValue, T_FALSE) != 1) {
